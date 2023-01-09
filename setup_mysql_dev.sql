6556c4df3f9eb596fa3178cb1e8459b863f833d6
samariwa <samuelmariwa@gmail.com>
2023-01-09 08:44:35 -0800

    SQL script fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,6 @@
--- SQL script for setting up the mysql development server
--- This will be used in production of the airbnb clone project
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
-CREATE USER IF NOT EXISTS 'hbnb_dev_db'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+-- a script that prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
---
+++ test.sql
@@ -0,0 +1,3 @@
+Create database + drop user
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS hbnb_dev;
